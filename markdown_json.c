d8b8d870b3ed6e76ef71f4384832630f17658735
Fyodor Sheremetyev <sheremetyev@gmail.com>
2012-10-19 19:34:39 +0100

    Correct indents.

--- markdown_json.c
+++ markdown_json.c
@@ -15,53 +15,53 @@ void print_json_list(GString *out, element *elt, int indent);
 
 void print_json_element(GString *out, element *elt, int indent) {
   char * key;
-    switch (elt->key) {
-      case LIST:               key = "LIST"; break;
-      case RAW:                key = "RAW"; break;
-      case SPACE:              key = "SPACE"; break;
-      case LINEBREAK:          key = "LINEBREAK"; break;
-      case ELLIPSIS:           key = "ELLIPSIS"; break;
-      case EMDASH:             key = "EMDASH"; break;
-      case ENDASH:             key = "ENDASH"; break;
-      case APOSTROPHE:         key = "APOSTROPHE"; break;
-      case SINGLEQUOTED:       key = "SINGLEQUOTED"; break;
-      case DOUBLEQUOTED:       key = "DOUBLEQUOTED"; break;
-      case STR:                key = "STR"; break;
-      case LINK:               key = "LINK"; break;
-      case IMAGE:              key = "IMAGE"; break;
-      case CODE:               key = "CODE"; break;
-      case HTML:               key = "HTML"; break;
-      case EMPH:               key = "EMPH"; break;
-      case STRONG:             key = "STRONG"; break;
-      case PLAIN:              key = "PLAIN"; break;
-      case PARA:               key = "PARA"; break;
-      case LISTITEM:           key = "LISTITEM"; break;
-      case BULLETLIST:         key = "BULLETLIST"; break;
-      case ORDEREDLIST:        key = "ORDEREDLIST"; break;
-      case H1:                 key = "H1"; break;
-      case H2:                 key = "H2"; break;
-      case H3:                 key = "H3"; break;
-      case H4:                 key = "H4"; break;
-      case H5:                 key = "H5"; break;
-      case H6:                 key = "H6"; break;
-      case BLOCKQUOTE:         key = "BLOCKQUOTE"; break;
-      case VERBATIM:           key = "VERBATIM"; break;
-      case HTMLBLOCK:          key = "HTMLBLOCK"; break;
-      case HRULE:              key = "HRULE"; break;
-      case REFERENCE:          key = "REFERENCE"; break;
-      case NOTE:               key = "NOTE"; break;
-      case DEFLIST:            key = "DEFLIST"; break;
-      default:                 key = "?";
-    }
-    for (int i = 0; i < indent; i++)
-      g_string_append_printf(out, " ");
-    if ( elt->key == STR ) {
-      g_string_append_printf(out, "%p: %s   '%s'\n", elt, key, elt->contents.str);
-    } else {
-      g_string_append_printf(out, "%p: %s\n", elt, key);
-    }
-    if (elt->children)
-      print_json_list(out, elt->children, indent + 4);
+  switch (elt->key) {
+    case LIST:               key = "LIST"; break;
+    case RAW:                key = "RAW"; break;
+    case SPACE:              key = "SPACE"; break;
+    case LINEBREAK:          key = "LINEBREAK"; break;
+    case ELLIPSIS:           key = "ELLIPSIS"; break;
+    case EMDASH:             key = "EMDASH"; break;
+    case ENDASH:             key = "ENDASH"; break;
+    case APOSTROPHE:         key = "APOSTROPHE"; break;
+    case SINGLEQUOTED:       key = "SINGLEQUOTED"; break;
+    case DOUBLEQUOTED:       key = "DOUBLEQUOTED"; break;
+    case STR:                key = "STR"; break;
+    case LINK:               key = "LINK"; break;
+    case IMAGE:              key = "IMAGE"; break;
+    case CODE:               key = "CODE"; break;
+    case HTML:               key = "HTML"; break;
+    case EMPH:               key = "EMPH"; break;
+    case STRONG:             key = "STRONG"; break;
+    case PLAIN:              key = "PLAIN"; break;
+    case PARA:               key = "PARA"; break;
+    case LISTITEM:           key = "LISTITEM"; break;
+    case BULLETLIST:         key = "BULLETLIST"; break;
+    case ORDEREDLIST:        key = "ORDEREDLIST"; break;
+    case H1:                 key = "H1"; break;
+    case H2:                 key = "H2"; break;
+    case H3:                 key = "H3"; break;
+    case H4:                 key = "H4"; break;
+    case H5:                 key = "H5"; break;
+    case H6:                 key = "H6"; break;
+    case BLOCKQUOTE:         key = "BLOCKQUOTE"; break;
+    case VERBATIM:           key = "VERBATIM"; break;
+    case HTMLBLOCK:          key = "HTMLBLOCK"; break;
+    case HRULE:              key = "HRULE"; break;
+    case REFERENCE:          key = "REFERENCE"; break;
+    case NOTE:               key = "NOTE"; break;
+    case DEFLIST:            key = "DEFLIST"; break;
+    default:                 key = "?";
+  }
+  for (int i = 0; i < indent; i++)
+    g_string_append_printf(out, " ");
+  if ( elt->key == STR ) {
+    g_string_append_printf(out, "%p: %s   '%s'\n", elt, key, elt->contents.str);
+  } else {
+    g_string_append_printf(out, "%p: %s\n", elt, key);
+  }
+  if (elt->children)
+    print_json_list(out, elt->children, indent + 4);
 }
 
 void print_json_list(GString *out, element *elt, int indent) {
