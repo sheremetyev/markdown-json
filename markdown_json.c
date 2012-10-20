289a953ab7ce900d9c22b2a5a031ecff15ed7a9c
Fyodor Sheremetyev <sheremetyev@gmail.com>
2012-10-20 20:22:52 +0100

    Extract indent printing function.

--- markdown_json.c
+++ markdown_json.c
@@ -27,6 +27,12 @@ void print_json_string(GString *out, char* str) {
   }  
 }
 
+void print_indent(GString *out, int level) {
+  int i;
+  for (i = 0; i < level * 4; i++)
+    g_string_append_printf(out, " ");
+}
+
 // we return constant strings only
 char* key_name(int key) {
   switch (key) {
@@ -39,21 +45,18 @@ char* key_name(int key) {
   }
 }
 
-void print_json_list(GString *out, element *elt, int indent);
+void print_json_list(GString *out, element *elt, int level);
 
-void print_json_element(GString *out, element *elt, int indent) {
-  int i;
+void print_json_element(GString *out, element *elt, int level) {
   char * key = key_name(elt->key);
-  for (i = 0; i < indent; i++)
-    g_string_append_printf(out, " ");
+  print_indent(out, level);
   if ( elt->key == LINK || elt->key == IMAGE || elt->key == IMAGEBLOCK ) {
     g_string_append_printf(out, "[\"%s\", { \"URL\": \"", key);
     print_json_string(out, elt->contents.link->url);
     g_string_append_printf(out, "\" }");
-    print_json_list(out, elt->contents.link->label, indent + 4);
+    print_json_list(out, elt->contents.link->label, level + 1);
     g_string_append_printf(out, "\n");
-    for (i = 0; i < indent; i++)
-      g_string_append_printf(out, " ");
+    print_indent(out, level);
     g_string_append_printf(out, "]");
   } else if ( elt->contents.str != NULL ) {
     g_string_append_printf(out, "[\"%s\", \"", key);
@@ -63,16 +66,15 @@ void print_json_element(GString *out, element *elt, int indent) {
   } else {
     g_string_append_printf(out, "[\"%s\"", key);
     if (elt->children) {
-      print_json_list(out, elt->children, indent + 4);
+      print_json_list(out, elt->children, level + 1);
       g_string_append_printf(out, "\n");
-      for (i = 0; i < indent; i++)
-        g_string_append_printf(out, " ");
+      print_indent(out, level);
     }
     g_string_append_printf(out, "]", key);
   }
 }
 
-void print_json_list(GString *out, element *elt, int indent) {
+void print_json_list(GString *out, element *elt, int level) {
   while (elt != NULL) {
     if (elt->key == NOTELABEL || elt->key == REFERENCE ||
         (elt->key == NOTE && elt->contents.str != NULL)) {
@@ -81,13 +83,13 @@ void print_json_list(GString *out, element *elt, int indent) {
     }
 
     g_string_append_printf(out, ",\n");
-    print_json_element(out, elt, indent);
+    print_json_element(out, elt, level);
     elt = elt->next;
   }
 }
 
 void print_json_tree(GString *out, element *root) {
   g_string_append_printf(out, "[\"MARKDOWN\"");
-  print_json_list(out, root, 4);
+  print_json_list(out, root, 1);
   g_string_append_printf(out, "\n]\n");
 }
